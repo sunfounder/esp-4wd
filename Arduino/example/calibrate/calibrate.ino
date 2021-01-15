0bd1f295dcc6229bea4e6b20fa80639d8a9c601e
Star152 <1972133159@qq.com>
2021-01-15 10:49:25 +0800

    add example

---
+++ Arduino/example/calibrate/calibrate.ino
@@ -0,0 +1,13 @@
+#include "ESP32_RDP.h"
+
+Servo servo(27);
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  servo.set_angle(0);
+
+}
