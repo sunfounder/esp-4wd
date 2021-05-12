b04a2c1eb30d321d90dbb097cfdc63054e7485ee
Cavon <cavonxx@gmail.com>
2021-05-12 16:32:52 +0800

    fix typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_theme = 'sphinx-celery'
+html_theme = 'sphinx_celery'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 latex_engine = 'pdflatex'
